86c88a4820449106c75ecdc8bd2994819cd120f2
juniaraldi <aldinugraha708@gmail.com>
2021-12-29 16:08:55 +0700

    first commit

---
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ProductCategory;
+
+class ProductCategoryController extends Controller
+{
+    public function all(Request $request)
+    {
+
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $show_product = $request->input('show_product');
+
+
+        if($id)
+        {
+            $category = ProductCategory::with(['products'])->find($id);
+
+            if($category){
+                return ResponseFormatter::success(
+                    $category,
+                    'Data kategori Berhasil Diambil'
+                );
+            }
+            else{
+                return ResponseFormatter::error(
+                    null,
+                    'Data kategori Tidak Ada',
+                    404
+                );
+            }
+        }
+
+        $category = ProductCategory::query();
+
+        if($name){
+            $category->where('name', 'Like', '%' . $name . '%');
+        }
+
+        if($show_product){
+            $category->with('products');
+        }
+
+        return ResponseFormatter::success(
+            $category->paginate($limit),
+            'Data Kategori Berhasil Diambil'
+        );
+    }
+}
