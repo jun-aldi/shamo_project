622432233666d44ba5ae4a99207d68b7a69155fe
juniaraldi <aldinugraha708@gmail.com>
2022-01-06 14:40:52 +0700

    datatables - api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductCategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function(){
+    Route::name('dashboard.')->prefix('dashboard')->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+        Route::middleware(['admin'])->group(function () {
+            Route::resource('category', ProductCategoryController::class);
+        });
+    });
+});
